54e77c2fdca41bb4c548916270b59c7b7178cb1d
hendycap <201559153+hendycap@users.noreply.github.com>
2025-05-17 10:45:59 +0000

    Deploying to gh-pages from @ Kazanya/docs@33d4c8489862a47343ef6d1ebf7a5ca10a7ea9ff 🚀

--- _nuxt/BWri_pge.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./Byv5TmER.js";import t from"./wzvqBaf1.js";import{z as e,H as a,i as p,w as c,V as i}from"./CZogK5-O.js";import"./C2bHYaEW.js";const d={__name:"ProseTip",setup(n){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:i(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
