be4e9d852e6ab31d604676a4166432029a3531ee
hendycap <201559153+hendycap@users.noreply.github.com>
2025-05-17 12:42:02 +0000

    Deploying to gh-pages from @ Kazanya/docs@9f4f9f32be9149f1358b32884d8200e0320e7b16 🚀

---
+++ _nuxt/BbvabLlL.js
@@ -0,0 +1,1 @@
+import{r as s}from"./BSuPt_iJ.js";import t from"./Da_HC-_V.js";import{z as e,H as a,i as p,w as c,V as i}from"./B95VHKeZ.js";import"./C2bHYaEW.js";const d={__name:"ProseTip",setup(n){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:i(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
--- _nuxt/BcRlJhME.js
+++
@@ -1,1 +0,0 @@
-import{r as n}from"./5o_a3wcD.js";import a from"./eFD2VM5k.js";import{z as s,H as t,i as e,w as i,V as p}from"./CSpitUY9.js";import"./C2bHYaEW.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
